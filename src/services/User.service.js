f5f0ea0d61b4b2b963901ff1c41e7eb8bbe5859d
leocaridade <leocaridade@hotmail.com>
2023-07-13 19:01:01 -0400

    fix: lint

--- src/services/User.service.js
+++ src/services/User.service.js
@@ -3,5 +3,5 @@ const { User } = require('../models');
 const getByEmail = (email) => User.findOne({ where: { email } });
 
 module.exports = {
-  getByEmail
-}
+  getByEmail,
+};
--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,5 @@
 const UserService = require('./User.service');
 
 module.exports = {
-  UserService
-}
+  UserService,
+};
